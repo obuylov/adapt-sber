7a21a5b5d9b5fd670922fa3b3425f7d8dbddc116
Олег Буйлов <bom-1990@yandex.ru>
2020-08-17 20:05:50 +0300

    Добавление компонентов, расширений, меню и темы

--- grunt/config/connect.js
+++ grunt/config/connect.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt, options) {
 
-  var port = grunt.option('port') || 9099;
+  var port = grunt.option('port') || 9001;
   var host = grunt.option('host') || 'localhost';
 
   return {
