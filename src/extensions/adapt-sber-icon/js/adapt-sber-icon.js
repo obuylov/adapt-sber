bfd52662dd23aac030ccd4e171567ba215937ba0
mikhail <michas-yoo@list.ru>
2020-12-05 01:48:44 +0300

    Icons update

--- src/extensions/adapt-sber-icon/js/adapt-sber-icon.js
+++ src/extensions/adapt-sber-icon/js/adapt-sber-icon.js
@@ -6,7 +6,7 @@ define([
 
     initialize: function() {
       const current_components = this.model.findDescendantModels("component");
-      this.component_models = current_components.filter(el => el.get("_sberIcon"));
+      this.component_models = current_components.filter(el => el.get("_sberIcon")._isEnabled);
 
       if (this.component_models.length === 0) return false;
 
@@ -18,9 +18,14 @@ define([
         let id = component.get("_id");
         let el = this.$("." + id)[0];
         let config = component.get("_sberIcon");
-        let body = el.querySelector(".component__body");
+        let element_to_append = component.get("_component") === "text" ? "body" : "widget";
+        let body = el.querySelector(".component__" + element_to_append);
+
+        if (element_to_append === 'widget' && body == null)
+          body = el.querySelector(".component-widget");
 
         body.classList.add("sber-icon-" + config.side);
+        body.style.alignItems = config.align;
 
         let img = this.createImage(config);
         body.prepend(img);
