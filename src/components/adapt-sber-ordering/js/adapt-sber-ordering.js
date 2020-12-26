b7008157a394aa263d6eef0d35b88a5f16859e3b
mikhail <michas-yoo@list.ru>
2020-12-26 23:28:04 +0300

    Сбер порядок

---
+++ src/components/adapt-sber-ordering/js/adapt-sber-ordering.js
@@ -0,0 +1,103 @@
+define([
+  'core/js/adapt',
+  'core/js/views/questionView',
+  'core/js/models/questionModel',
+  './jqueryUi'
+], function (Adapt, QuestionView, QuestionModel, ui) {
+
+  class SberOrderingView extends QuestionView {
+    preRender() {
+      this.resetAnswers();
+    }
+
+    resetAnswers() {
+      this.answers = [];
+      this.items = this.model.get('_items');
+    }
+
+    postRender() {
+      this.setReadyStatus();
+
+      if (!this.model.get('_isComplete')) {
+        this.setupSortableLogic();
+        if (this.model.get('_isRandom')) {
+          this.setupRandomIndices();
+        }
+      } else {
+        this.addDisabledStyles();
+      }
+    }
+
+    events() {
+      return {
+        'click .btn__action': 'onSubmitClicked'
+      };
+    }
+
+    setupRandomIndices() {
+      let len = this.items.length;
+      // сколько элементов нужно подвинуть
+      let amount = Math.floor(len / 2);
+      // какие индексы свободны
+      let indices = [...Array(len).keys()];
+
+      // от 0 до количества перемещений
+      for (let i = 0; i < amount; i++) {
+        // берем случайный индекс
+        let idx = Math.floor(Math.random() * indices.length);
+        // двигаем элемент с этим индексом
+        this.$('.ordering__container')[0].prepend(this.$('.ordering__item').eq(indices[idx])[0]);
+        // удаляем индекс из доступных
+        indices.splice(idx, 1);
+      }
+    }
+
+    setupSortableLogic() {
+      this.$('.ordering__container').sortable({
+        axis: 'y'
+      });
+      this.$('.ordering__container').disableSelection();
+    }
+
+    onSubmitClicked() {
+      this.resetAnswers();
+
+      this.items.forEach((el) => {
+        this.answers.push(this.$('.ordering__item').eq(el.position).text().trim() === el.text);
+      });
+
+      this.checkAnswers();
+    }
+
+    checkAnswers() {
+      // есть ошибки
+      if (this.answers.indexOf(false) !== -1) {
+        this.showFeedback('incorrect');
+      } else {
+        this.showFeedback('correct');
+        this.setCompletionStatus();
+        this.addDisabledStyles();
+      }
+    }
+
+    showFeedback(what) {
+      Adapt.notify.popup({
+        title: this.model.get('_displayTitle'),
+        body: this.model.get('_feedback')[what],
+        _classes: 'ordering'
+      });
+    }
+
+    addDisabledStyles() {
+      this.$('button').attr('disabled', 'disabled');
+      this.$('button').addClass('disabled');
+    }
+  }
+
+  SberOrderingView.template = 'sber-ordering';
+
+  return Adapt.register('sber-ordering', {
+    model: QuestionModel.extend({}),// create a new class in the inheritance chain so it can be extended per component type if necessary later
+    view: SberOrderingView
+  });
+});
