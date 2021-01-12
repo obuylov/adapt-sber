a83548848150668a05194bdae94a0d840be59a33
mikhail <michas-yoo@list.ru>
2021-01-13 01:01:36 +0300

    sber-yes-no

---
+++ src/components/adapt-sber-yes-no/js/adapt-sber-yes-no.js
@@ -0,0 +1,59 @@
+define([
+  'core/js/adapt',
+  'core/js/views/componentView',
+  'core/js/models/componentModel'
+], function (Adapt, ComponentView, ComponentModel) {
+
+  class SberYesNoView extends ComponentView {
+    events() {
+      return {
+        'click .btn-text': 'checkAnswer',
+      };
+    }
+
+    postRender() {
+      this.setReadyStatus();
+    }
+
+    checkAnswer(event) {
+      let el = event.target;
+      // если пользователь нажал на "Да", то условие пройдет, и user_answered_yes будет true
+      // а иначе, оно будет false
+      let user_answered_yes = el.dataset.answer === 'yes';
+      let feedback = this.model.get('_feedback');
+
+      // если нужно выбрать, и нажали на кнопку "да", в условии будет true === true
+      // если выбирать не нужно, а мы нажали на "да", в условии будет false === true
+      // если нужно выбрать, а мы не нажали на "да", в условии будет true === false
+      if (this.model.get('_shouldBeSelected') === user_answered_yes) {
+        this.showFeedback(feedback._correct_title, feedback._correct);
+      } else {
+        this.showFeedback(feedback._incorrect_title, feedback._incorrect);
+      }
+
+      this.disableButtons();
+      this.setCompletionStatus();
+    }
+
+    showFeedback(title, body) {
+      Adapt.notify.popup({
+        title,
+        body
+      });
+    }
+
+    disableButtons() {
+      this.$('button').each(function () {
+        $(this).addClass('disabled');
+        $(this).attr('disabled', 'disabled');
+      });
+    }
+  }
+
+  SberYesNoView.template = 'sber-yes-no';
+
+  return Adapt.register('sber-yes-no', {
+    model: ComponentModel.extend({}),// create a new class in the inheritance chain so it can be extended per component type if necessary later
+    view: SberYesNoView
+  });
+});
--- src/theme/adapt-sber-vanilla/js/updatedNarrativeLogic.js
+++ src/theme/adapt-sber-vanilla/js/updatedNarrativeLogic.js
@@ -15,6 +15,9 @@ define([
     },
 
     updateCurrentItem: function() {
+      if (!this.$el.hasClass('_hasDigitNavigation')) {
+        return;
+      }
       let currentItemIndex = this.$el.find(".narrative__content-item:not(.u-display-none)").data("index") + 1;
       let allElements = this.model.attributes.attributes._items.length;
       this.el.querySelector(".narrative__indicators").innerHTML = `<span class="narrative__current-element">${currentItemIndex} / ${allElements}</span>`;
