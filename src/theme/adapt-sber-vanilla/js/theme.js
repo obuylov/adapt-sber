707d20329af429a9f75349fa810166cfcddf45b8
mikhail <michas-yoo@list.ru>
2021-03-09 20:42:10 +0300

    updates

--- src/theme/adapt-sber-vanilla/js/theme.js
+++ src/theme/adapt-sber-vanilla/js/theme.js
@@ -39,25 +39,28 @@ define([
 
     if (view.model.get('_component') === 'assessmentResults') {
       let id = view.model.findAncestor('article').get('_id');
+      let v_id = view.model.get('_id');
 
-      function updateVisibility() {
-        if (view.model.get('_isVisible')) {
-          $('.' + id).removeClass('dn');
+      function updateVisibility(id) {
+        let art = $('.' + id).parents('.article');
 
-          let figures = Adapt.findById(id).get('_sberFigures');
+        if (Adapt.findById(id).get('_isVisible')) {
+          art.removeClass('dn');
+
+          let figures = Adapt.findById(id).findAncestor('article').get('_sberFigures');
           if (figures._isEnabled) {
             for (let i in figures._items) {
               let el = figures._items[i];
 
-              $(`.${id} svg`).eq(i).css('top', el.y_pos + '%');
+              art.find('svg').eq(i).css('top', el.y_pos + '%');
             }
           }
         } else {
-          $('.' + id).addClass('dn');
+          art.addClass('dn');
         }
       }
 
-      updateVisibility();
+      updateVisibility(v_id);
 
       let theObject = Adapt.offlineStorage.get('currentClassName');
       theObject = theObject ? theObject : {};
@@ -67,14 +70,20 @@ define([
         $('.' + id).addClass(currentClassName);
       }
 
-      Adapt.listenTo(view.model, 'change:_isVisible', () => {
-        updateVisibility();
+      Adapt.listenTo(view.model, 'change:_isVisible', function () {
+        updateVisibility(v_id);
 
         let matchedClass = $('.article.' + id).attr('class').match(/quiz-(in)?correct/g);
+        if (matchedClass && matchedClass.length > 1) {
+          matchedClass.shift();
+        }
+
         theObject[id] = matchedClass ? matchedClass[0] : '';
         Adapt.offlineStorage.set('currentClassName', theObject);
       });
-      Adapt.listenTo(Adapt, 'pageView:postRender', updateVisibility);
+      Adapt.listenTo(Adapt, 'pageView:postRender', () => {
+        updateVisibility(v_id);
+      });
     }
 
     if (view.model.get('_component') === 'textinput') {
